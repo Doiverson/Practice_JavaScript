25590a39a1d919b9086ec0c3f6bb30f50534ff1c
Your Name <doiverson.canada@gmail.com>
2018-10-09 23:34:17 -0700

    add function to use local strage

---
+++ Practice_6_localStorage/app.js
@@ -0,0 +1,20 @@
+
+
+function inputName(){
+    const text = document.getElementById('test').value;
+    console.log(text);
+    localStorage.setItem("Name", text);
+    console.log("What you input is " + text);
+}
+
+function outputName(){
+    const stragedName = localStorage.getItem("Name");
+    console.log("Output is " + stragedName);
+    document.getElementById('test').value = stragedName;
+}
+
+function changeColor(){
+    let a = document.getElementById('colorChange');
+    let b = document.getElementById('test');
+    b.style.color =a.value;
+}
